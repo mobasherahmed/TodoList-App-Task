f22999de6e984f49d7177faa4d7bce0d8cf8493f
maged.a.hussien <maged.a.hussien@gmail.com>
2020-02-29 13:35:24 +0200

    todoListAppTask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,45 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-app';
+  
+
+  todoArray = [];
+  todo;
+  todoUpdate:boolean = false;
+  
+  
+  /* add todo */
+  addTodo(value) {
+    if (value !== "" && value!== " " ) {
+      this.todoArray.push(value)
+    }
+    else {
+      alert('Field required **')
+    }
+  }
+
+  /*delete item*/
+  deleteItem(todo) {
+    this.todoUpdate = false;
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
+
+  todoSubmit(value: any) {
+    if (value.todo !== '' && value.todo !== ' ' ) {
+      this.todoArray.push(value.todo) 
+    } else {
+      alert('Field required **')
+    }
+  }
+
 }
+
+
+
+
